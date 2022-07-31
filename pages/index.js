9400483ef29fcd20aa43750100102b63ecbb89a7
Tom Gandolfo <tomgandolfo2@gmail.com>
2022-07-31 12:21:25 -0300

    Commit 3

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div className="flex place-content-between bg-gray-200">
+            <div className="p-4 m-4 bg-orange-600 rounded-2xl text-white font-bold">Logo</div>
+            <div className="flex">
+                <div className="p-4 m-4 bg-orange-600 rounded-2xl text-white font-bold">Home</div>
+                <div className="p-4 m-4 bg-orange-600 rounded-2xl text-white font-bold">
+                    Address
+                </div>
+                <div className="p-4 m-4 bg-orange-600 rounded-2xl text-white font-bold">
+                    Balance
+                </div>
+            </div>
+            <div className="p-4 m-4 bg-blue-600 hover:bg-blue-400 rounded-2xl text-white font-bold">
+                Connect
+            </div>
+        </div>
+    )
 }
